aa68689f00b43819320df4ec624cb3e5f3905cfa
0xfb99 <koppel444@gmx.de>
2022-06-14 14:42:42 +0200

    init

---
+++ tenderly/main.js
@@ -0,0 +1,7 @@
+txIdTag = "spanTxHash"
+tag = document.getElementById(txIdTag)
+hash = tag.innerHTML
+link = "https://dashboard.tenderly.co/tx/mainnet/" + hash
+document.getElementById(txIdTag).innerHTML = '<a href="' + link + '" target="_blank">' + hash + '></a>'
+
+
