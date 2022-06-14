5caa4b5a00d914e15b70eb709f306bde919056ae
0xfb99 <koppel444@gmx.de>
2022-06-14 14:46:05 +0200

    typo

--- tenderly/main.js
+++ tenderly/main.js
@@ -2,6 +2,6 @@ txIdTag = "spanTxHash"
 tag = document.getElementById(txIdTag)
 hash = tag.innerHTML
 link = "https://dashboard.tenderly.co/tx/mainnet/" + hash
-document.getElementById(txIdTag).innerHTML = '<a href="' + link + '" target="_blank">' + hash + '></a>'
+document.getElementById(txIdTag).innerHTML = '<a href="' + link + '" target="_blank">' + hash + '</a>'
 
 
